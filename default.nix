b08ffee2fd0e11643ceac3b1751a5197464cbeb2
Maksymilian Owsianny <Maksymilian.Owsianny@gmail.com>
2019-01-24 23:19:31 +0000

    Initial commit.

---
+++ default.nix
@@ -0,0 +1,42 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghc843" }:
+let ghc = nixpkgs.haskell.packages.${compiler}.override {
+      overrides = self: super: {
+        # TODO: get packages from github
+        # generic-lens-labels = loadLocal self "generic-lens-labels";
+        # named               = loadLocal self "named";
+        # concat-satisfy        = loadLocal self "concat/satisfy";
+        # concat-known          = loadLocal self "concat/known";
+        # concat-inline         = loadLocal self "concat/inline";
+        # concat-classes        = loadLocal self "concat/classes";
+        # concat-plugin         = loadLocal self "concat/plugin";
+        # concat-graphics       = loadLocal self "concat/graphics";
+        # concat-examples       = loadLocal self "concat/examples";
+
+        halive          = loadDirec self "${~/Projects/Gridlock/halive}";
+        Carnot          = loadDirec self "${./../Carnot}";
+      };
+    };
+
+    overrideDeriv = drv: f: drv.override (args: args // {
+      mkDerivation = drv: args.mkDerivation (drv // f drv);
+    });
+    dontCheck = drv: overrideDeriv drv (drv: { doCheck = false; });
+
+    loadLocal = self: name:
+      self.callPackage (cabal2nixResult (./deps + "/${name}")) {};
+    loadDirec = self: name:
+      self.callPackage (cabal2nixResult (name)) {};
+    tools = with ghc; [ cabal-install ghcid halive ];
+    overrideCabal = pkg: nixpkgs.haskell.lib.overrideCabal pkg
+      ({buildDepends ? [], ...}: {
+        buildDepends = buildDepends ++ tools;
+      });
+    cabal2nixResult = src: nixpkgs.runCommand "cabal2nixResult" {
+      buildCommand = ''
+        cabal2nix --no-check --jailbreak --no-haddock file://${src} > $out
+      '';
+      buildInputs = [ nixpkgs.cabal2nix ];
+    } "";
+    package = ghc.callPackage (cabal2nixResult ./.) { };
+    drv = overrideCabal package;
+in if nixpkgs.lib.inNixShell then drv.env else drv
